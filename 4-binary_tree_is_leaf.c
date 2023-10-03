9eb12ea74ae33fb699dbed3dab11583270efe4f2
wambui-g <grayciekamau03@gmail.com>
2023-10-03 04:29:19 -0700

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,5 +8,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	return ((!node || node->left || node->right) ? 0 : 1);
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
