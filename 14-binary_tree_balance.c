451521697772b6be49eddfddda47a02496e8f4b6
wambui-g <grayciekamau03@gmail.com>
2023-10-05 02:31:59 -0700

    103

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance- function to measure balance facor of binary
+ * @tree: pointer to the root node of tree
+ *
+ * Return: int
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+}
